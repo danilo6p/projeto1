b0515bb2c0fa415932b1625a402d6a64f57e568b
Danilo Abreu <daabreu@sga.pucminas.br>
2022-05-15 21:51:49 -0300

    usando express para as rotas

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end("ola da danki code !");
-
-});
-
-server.listen(port, hostname, () => {
-    console.log('servidor rodando !');
+router.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'index.html'));
 
 });
+
+app.use('/',router);
+
+app.listen(process.env.port || 3000);
